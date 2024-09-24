5f1c7fc3abe50b98e0946011d8e9f9ba4526d597
xiuxiu62 <jacremer@live.com>
2024-09-24 00:07:07 -0700

    adds slice impl and starts parallax impl

---
+++ src/collections/slice.h
@@ -0,0 +1,38 @@
+#include "core/types.h"
+
+template <typename T> struct Slice {
+    T *ptr;
+    usize len;
+
+    T *begin();
+    T *end();
+    const T *begin() const;
+    const T *end() const;
+
+    T &operator[](usize i);
+    const T &operator[](usize i) const;
+};
+
+template <typename T> T *Slice<T>::begin() {
+    return ptr;
+}
+
+template <typename T> T *Slice<T>::end() {
+    return ptr + sizeof(T) * len;
+}
+
+template <typename T> const T *Slice<T>::begin() const {
+    return ptr;
+}
+
+template <typename T> const T *Slice<T>::end() const {
+    return ptr + sizeof(T) * len;
+}
+
+template <typename T> T &Slice<T>::operator[](usize i) {
+    return i < len ? ptr[i] : nullptr;
+}
+
+template <typename T> const T &Slice<T>::operator[](usize i) const {
+    return i < len ? ptr[i] : nullptr;
+}
