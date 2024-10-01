b32cf733e684e1782c3826b29e13775c3651c63f
xiuxiu62 <jacremer@live.com>
2024-09-30 20:24:38 -0700

    organizes component system

--- src/entity.cc
+++ src/entity.cc
@@ -1,13 +1,13 @@
 #include "entity.h"
 #include "collections/generational_arena.h"
+#include "core/types.h"
 #include "graphics/light/point.h"
 #include "math/transform.h"
 #include "physics/body.h"
 #include "sprite/sprite.h"
+#include <cstring>
 
-#define MAX_NAME_LENGTH 32
-
-typedef GenHandle EntityHandle;
+#define MAX_LABEL_LENGTH 64
 
 enum ComponentKind {
     TRANSFORM = 0,
@@ -23,6 +23,7 @@ static struct {
 } storage;
 
 struct Entity {
+    const u8 label[MAX_LABEL_LENGTH];
     u32 mask;
     GenHandle component_handles[COMPONENT_COUNT];
 };
@@ -60,25 +61,50 @@ void entity_despawn(GenHandle handle) {
     gen_arena_remove(storage.entities, handle); // Deinitialize entity;
 }
 
-u8 *entity_insert(GenHandle handle, ComponentKind kind) {
+template <typename T> const T *entity_get(GenHandle handle, ComponentKind kind) {
     Entity *entity = reinterpret_cast<Entity *>(gen_arena_get(storage.entities, handle));
-    if (!entity) return nullptr; // Ensure the entity exists
+    if (!entity || !(entity->mask & (1 << kind))) return nullptr;
 
-    // Insert a new component
-    GenHandle component_handle = gen_arena_insert(storage.components[kind], nullptr);
-
-    // Update entity with component info
-    entity->mask |= (1 << kind);
-    entity->component_handles[kind] = component_handle;
-
-    // Query for component
-    return gen_arena_get(storage.components[kind], component_handle);
+    GenHandle component_handle = entity->component_handles[kind];
+    return reinterpret_cast<T *>(gen_arena_get(storage.components[kind], component_handle));
 }
 
-u8 *entity_get_component(GenHandle handle, ComponentKind kind) {
+template <typename T> T *entity_get_mut(GenHandle handle, ComponentKind kind) {
     Entity *entity = reinterpret_cast<Entity *>(gen_arena_get(storage.entities, handle));
     if (!entity || !(entity->mask & (1 << kind))) return nullptr;
 
     GenHandle component_handle = entity->component_handles[kind];
     return gen_arena_get(storage.components[kind], component_handle);
+}
+
+template <typename T> void entity_insert(GenHandle handle, ComponentKind kind, T data) {
+    Entity *entity = reinterpret_cast<Entity *>(gen_arena_get(storage.entities, handle));
+    if (!entity) return; // Ensure the entity exists
+
+    // Insert a new component
+    GenHandle component_handle = gen_arena_insert(storage.components[kind], reinterpret_cast<u8 *>(&data));
+
+    // Update entity with component info
+    entity->mask |= (1 << kind);
+    entity->component_handles[kind] = component_handle;
+}
+
+template <typename T> void entity_update(GenHandle handle, ComponentKind kind, T data) {
+    Entity *entity = reinterpret_cast<Entity *>(gen_arena_get(storage.entities, handle));
+    if (!entity) return; // Ensure the entity exists
+
+    u32 stride = storage.components[kind].stride;
+    u8 *component = gen_arena_get(storage.components[kind], entity->component_handles[kind]);
+    memcpy(component, data, stride);
+}
+
+void temp() {
+    GenHandle character = entity_spawn();
+    Sprite sprite;
+    RigidBody body;
+
+    entity_insert(character, ComponentKind::SPRITE, sprite);
+    entity_insert(character, ComponentKind::SPRITE, body);
+
+    Sprite &temp = entity_get<Sprite>(character, ComponentKind::SPRITE);
 }
--- src/entity.h
+++ src/entity.h
@@ -2,14 +2,14 @@
 
 #include "collections/generational_arena.h"
 
-void entity_manager_init();
+// void entity_manager_init();
 
-void entity_manager_deinit();
+// void entity_manager_deinit();
 
-GenHandle entity_manager_spawn();
+// GenHandle entity_manager_spawn();
 
-bool entity_manager_despawn(GenHandle handle);
+// bool entity_manager_despawn(GenHandle handle);
 
-const u8 *entity_manager_get(GenHandle handle);
+// const u8 *entity_manager_get(GenHandle handle);
 
-u8 *entity_manager_get_mut(GenHandle handle);
+// u8 *entity_manager_get_mut(GenHandle handle);
